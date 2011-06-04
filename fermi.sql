aba79f7dc9dd5b6e281e8b401915c1387061012f
ephimetheus <ephimetheuss@gmail.com>
2011-06-04 17:53:12 +0200

    SQL Dump updated

--- fermi.sql
+++ fermi.sql
@@ -1,19 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 23. Mai 2011 um 16:39
--- Server Version: 5.5.8
--- PHP-Version: 5.3.5
+-- Erstellungszeit: 04. Juni 2011 um 15:52
+-- Server Version: 5.1.44
+-- PHP-Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Datenbank: `fermi`
@@ -28,18 +22,19 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `fermi_role` (
   `role_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `parent_id` set('1') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `parent_id` tinyint(3) unsigned DEFAULT NULL,
   PRIMARY KEY (`role_id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Daten für Tabelle `fermi_role`
 --
 
 INSERT INTO `fermi_role` (`role_id`, `name`, `parent_id`) VALUES
-(1, 'default', NULL),
-(2, 'admin', '1');
+(1, 'default', 4),
+(2, 'admin', 1),
+(4, 'super', NULL);
 
 -- --------------------------------------------------------
 
@@ -49,19 +44,18 @@ INSERT INTO `fermi_role` (`role_id`, `name`, `parent_id`) VALUES
 
 CREATE TABLE IF NOT EXISTS `fermi_role_user` (
   `role_user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `role_id` set('1') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `role_id` tinyint(3) unsigned DEFAULT NULL,
   `user_id` tinyint(3) unsigned DEFAULT NULL,
   PRIMARY KEY (`role_user_id`),
   UNIQUE KEY `UQ_e25d3cdf36978e9b84d1e0e4732b23444d9575aa` (`role_id`,`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 --
 -- Daten für Tabelle `fermi_role_user`
 --
 
 INSERT INTO `fermi_role_user` (`role_user_id`, `role_id`, `user_id`) VALUES
-(1, '1', 1),
-(2, '1', 2);
+(4, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -94,9 +88,10 @@ CREATE TABLE IF NOT EXISTS `fermi_site` (
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `content` text COLLATE utf8_unicode_ci,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `author_id` int(11) NOT NULL,
+  `author_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`site_id`),
-  UNIQUE KEY `name` (`name`)
+  UNIQUE KEY `name` (`name`),
+  KEY `author_id` (`author_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=26 ;
 
 --
@@ -104,9 +99,9 @@ CREATE TABLE IF NOT EXISTS `fermi_site` (
 --
 
 INSERT INTO `fermi_site` (`site_id`, `name`, `content`, `title`, `author_id`) VALUES
-(6, 'index', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 'Home', 0),
-(12, 'hans', 'Serious business', 'About', 0),
-(25, 'test', 'blaaa', 'Testseite', 0);
+(6, 'index', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 'Home', 1),
+(12, 'hans', 'Serious business', 'About', 1),
+(25, 'test', 'blaaa', 'Testseite', 1);
 
 -- --------------------------------------------------------
 
@@ -132,3 +127,13 @@ CREATE TABLE IF NOT EXISTS `fermi_user` (
 INSERT INTO `fermi_user` (`user_id`, `email`, `pass`, `salt`, `name`) VALUES
 (1, 'ephimetheuss@gmail.com', 'admin', 'asd476', 'paul'),
 (2, 'hans@email.de', 'admin', 'asd476', 'hans');
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `fermi_site`
+--
+ALTER TABLE `fermi_site`
+  ADD CONSTRAINT `fermi_site_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `fermi_user` (`user_id`);
